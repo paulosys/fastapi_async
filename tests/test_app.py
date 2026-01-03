242ba3805d69542ce6cc7b012e2ea8d1ec14ee56
paulosys <paulosilvasergio486@gmail.com>
2026-01-03 15:24:52 -0300

    chore: setup FastAPI project with basic structure and tests
    - Added pyproject.toml for project configuration and dependencies management.
    - Created src directory with initial FastAPI application (app.py).
    - Added tests directory with a test for the root endpoint.
    - Included development dependencies for linting and testing.

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from src.app import app
+
+client = TestClient(app)
+
+
+def test_read_root_should_return_hello_world():
+    response = client.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Hello, World!'}
